ec74335ce68792bdce8fdaf76c23d70bcd3708a2
SeyyedAhmad <seyyedahmadrokni@ut.ac.ir>
2025-05-01 21:19:58 +0330

    fix(codes): before simplifying the codes

--- Codes/edge_1/edge_1.ino
+++ Codes/edge_1/edge_1.ino
@@ -1,23 +1,35 @@
-#include <Wire.h>
 #include <Servo.h>
-
+#include <Wire.h>
 // مشخصات سخت افزاری
-#define SOIL_SENSOR_PIN A0  // پین سنسور رطوبت
-#define DC_MOTOR_PIN1 8      // برای کنترل L298N - IN1
-#define SERVO_PIN 9          // پین موتور سروو
+// #define SOIL_SENSOR_PIN A0  // پین سنسور رطوبت
+// #define DC_MOTOR_PIN1 8      // برای کنترل L298N - IN1
+// #define SERVO_PIN 9          // پین موتور سروو
 
-Servo potServo; // سروو برای چرخش گلدان
+// Servo potServo; // سروو برای چرخش گلدان
 
-int soil_moisture = 71; // متغیر برای رطوبت خاک
+uint8_t soil_moisture = 170; // متغیر برای رطوبت خاک
+byte zero = 0;
+// ارسال داده به Master
+void sendData() {
+  Serial.println("Request received");
+  Wire.write(soil_moisture);
+  uint8_t output = Wire.write(zero);
+  Serial.print("soil moisture : ");
+  Serial.println(soil_moisture);
+  Serial.print("output : ");
+  Serial.println(output);
+}
 
 void setup() {
-  Wire.begin(23);
-  Wire.onReceive(receiveData); // وقتی Master داده فرستاد
+  Wire.begin(8);
+  // pinMode(A4, INPUT_PULLUP);
+  // pinMode(A5, INPUT_PULLUP);
   Wire.onRequest(sendData);    // وقتی Master درخواست داده کرد
+  // Wire.onReceive(receiveData); // وقتی Master داده فرستاد
 
-  potServo.attach(SERVO_PIN);
+  // potServo.attach(SERVO_PIN);
 
-  pinMode(DC_MOTOR_PIN1, OUTPUT);
+  // pinMode(DC_MOTOR_PIN1, OUTPUT);
 
   Serial.begin(9600);
   Serial.println("edge 1 started");
@@ -25,9 +37,9 @@ void setup() {
 
 void loop() {
   // هر ثانیه رطوبت خاک رو بخون
-  int rawMoisture = analogRead(SOIL_SENSOR_PIN);
-  soil_moisture = map(rawMoisture, 0, 1023, 0, 100);
-  Serial.println(soil_moisture);
+  // int rawMoisture = analogRead(SOIL_SENSOR_PIN);
+  // soil_moisture = map(rawMoisture, 0, 1023, 0, 100);
+  // Serial.println(soil_moisture);
   // int data = Wire.read();
   // Serial.println(data);
   
@@ -35,50 +47,43 @@ void loop() {
 }
 
 // دریافت فرمان از Master
-void receiveData(int howMany) {
+// void receiveData(int howMany) {
   
-  while(Wire.available()) {
-    char command = Wire.read();
-    if (command == -1){
-      continue;
-    }
-    if(command == 'W') {
-      // فرمان آبیاری
+//   while(Wire.available()) {
+//     char command = Wire.read();
+//     if (command == -1){
+//       continue;
+//     }
+//     if(command == 'W') {
+//       // فرمان آبیاری
 
-      Serial.println("Command W recieved");
-      // int irrigationRate = Wire.read(); // نرخ آبیاری بر اساس درصد
-      // Serial.print("irrigationRate :");
-      // Serial.println(irrigationRate);
+//       Serial.println("Command W recieved");
+//       // int irrigationRate = Wire.read(); // نرخ آبیاری بر اساس درصد
+//       // Serial.print("irrigationRate :");
+//       // Serial.println(irrigationRate);
 
-      // if (irrigationRate > 0) {
-      //   openWaterValve();
-      // } else {
-      //   closeWaterValve();
-      // }
-    }
-    else if(command == 'R') {
-      // فرمان چرخش گلدان
-      int rotation = Wire.read();
-      potServo.write(rotation);
-      Serial.print("rotation :");
-      Serial.println(rotation);
-    }
+//       // if (irrigationRate > 0) {
+//       //   openWaterValve();
+//       // } else {
+//       //   closeWaterValve();
+//       // }
+//     }
+//     else if(command == 'R') {
+//       // فرمان چرخش گلدان
+//       int rotation = Wire.read();
+//       potServo.write(rotation);
+//       Serial.print("rotation :");
+//       Serial.println(rotation);
+//     }
 
-  }
-}
-
-// ارسال داده به Master
-void sendData() {
-  Wire.write(sprintf("%d", soil_moisture)); // ارسال مقدار رطوبت
-  Serial.print("soil moisture :");
-  Serial.println(sprintf("%d", soil_moisture));
-}
+//   }
+// }
 
 // توابع کنترل آب
-void openWaterValve() {
-  digitalWrite(DC_MOTOR_PIN1, HIGH);
-}
+// void openWaterValve() {
+//   digitalWrite(DC_MOTOR_PIN1, HIGH);
+// }
 
-void closeWaterValve() {
-  digitalWrite(DC_MOTOR_PIN1, LOW);
-}
+// void closeWaterValve() {
+//   digitalWrite(DC_MOTOR_PIN1, LOW);
+// }
