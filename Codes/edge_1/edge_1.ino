50df5154d9da7f8c3d4ebe73331616414764e11d
SeyyedAhmad <seyyedahmadrokni@ut.ac.ir>
2025-05-01 22:48:29 +0330

    fix: slave to master connection fixed

--- Codes/edge_1/edge_1.ino
+++ Codes/edge_1/edge_1.ino
@@ -1,89 +1,20 @@
-#include <Servo.h>
 #include <Wire.h>
-// مشخصات سخت افزاری
-// #define SOIL_SENSOR_PIN A0  // پین سنسور رطوبت
-// #define DC_MOTOR_PIN1 8      // برای کنترل L298N - IN1
-// #define SERVO_PIN 9          // پین موتور سروو
+#define SLAVE_ADDR 0x08
 
-// Servo potServo; // سروو برای چرخش گلدان
-
-uint8_t soil_moisture = 170; // متغیر برای رطوبت خاک
-byte zero = 0;
-// ارسال داده به Master
-void sendData() {
-  Serial.println("Request received");
-  Wire.write(soil_moisture);
-  uint8_t output = Wire.write(zero);
-  Serial.print("soil moisture : ");
-  Serial.println(soil_moisture);
-  Serial.print("output : ");
-  Serial.println(output);
-}
+byte data_to_send = 150;
 
 void setup() {
-  Wire.begin(8);
-  // pinMode(A4, INPUT_PULLUP);
-  // pinMode(A5, INPUT_PULLUP);
-  Wire.onRequest(sendData);    // وقتی Master درخواست داده کرد
-  // Wire.onReceive(receiveData); // وقتی Master داده فرستاد
-
-  // potServo.attach(SERVO_PIN);
-
-  // pinMode(DC_MOTOR_PIN1, OUTPUT);
-
+  Wire.begin(SLAVE_ADDR);
   Serial.begin(9600);
-  Serial.println("edge 1 started");
+  Wire.onRequest(sendData);
+  pinMode(A4, INPUT_PULLUP);
+  pinMode(A5, INPUT_PULLUP);
 }
 
 void loop() {
-  // هر ثانیه رطوبت خاک رو بخون
-  // int rawMoisture = analogRead(SOIL_SENSOR_PIN);
-  // soil_moisture = map(rawMoisture, 0, 1023, 0, 100);
-  // Serial.println(soil_moisture);
-  // int data = Wire.read();
-  // Serial.println(data);
-  
   delay(1000);
 }
 
-// دریافت فرمان از Master
-// void receiveData(int howMany) {
-  
-//   while(Wire.available()) {
-//     char command = Wire.read();
-//     if (command == -1){
-//       continue;
-//     }
-//     if(command == 'W') {
-//       // فرمان آبیاری
-
-//       Serial.println("Command W recieved");
-//       // int irrigationRate = Wire.read(); // نرخ آبیاری بر اساس درصد
-//       // Serial.print("irrigationRate :");
-//       // Serial.println(irrigationRate);
-
-//       // if (irrigationRate > 0) {
-//       //   openWaterValve();
-//       // } else {
-//       //   closeWaterValve();
-//       // }
-//     }
-//     else if(command == 'R') {
-//       // فرمان چرخش گلدان
-//       int rotation = Wire.read();
-//       potServo.write(rotation);
-//       Serial.print("rotation :");
-//       Serial.println(rotation);
-//     }
-
-//   }
-// }
-
-// توابع کنترل آب
-// void openWaterValve() {
-//   digitalWrite(DC_MOTOR_PIN1, HIGH);
-// }
-
-// void closeWaterValve() {
-//   digitalWrite(DC_MOTOR_PIN1, LOW);
-// }
+void sendData(){
+  Wire.write(data_to_send);
+}
--- Codes/master_node/master_node.ino
+++ Codes/master_node/master_node.ino
@@ -1,122 +1,17 @@
-#include <string.h>
 #include <Wire.h>
-// Pin and sensor configuration
-// #define LIGHT_SENSOR_LEFT A1
-// #define LIGHT_SENSOR_RIGHT A2
-// #define TEMPERATURE_SENSOR A0
-
-#define EDGE_NODE_1 8
-// #define EDGE_NODE_2 0x11
+#define SLAVE_ADDR 0x08
 
 void setup() {
   Wire.begin();
   Serial.begin(9600);
-  Serial.println("Plant management system initialized");
 }
 
 void loop() {
-  // float temperature = analogRead(TEMPERATURE_SENSOR);
-  // Serial.print("temperature: ");
-  // Serial.println(temperature);
-  // int lightLeft = analogRead(LIGHT_SENSOR_LEFT);
-  // int lightRight = analogRead(LIGHT_SENSOR_RIGHT);
-  // Serial.print("sensor left: ");
-  // Serial.println(lightLeft);
-  // Serial.print("sensor right: ");
-  // Serial.println(lightRight);
-  // int optimalPosition = (lightLeft > lightRight) ? 0 : 60;
-  
-  // manageEdgeNode(EDGE_NODE_1, temperature, optimalPosition);
-  manageEdgeNode(EDGE_NODE_1, 3, 3);
-  // manageEdgeNode(EDGE_NODE_2, temperature, optimalPosition);
-  
+  Wire.requestFrom(SLAVE_ADDR, 1);
+  if(Wire.available()) {
+    byte data = Wire.read();
+    // Serial.print("Received: ");
+    Serial.println(data);
+  }
   delay(1000);
 }
-
-void manageEdgeNode(int nodeAddress, float temperature, int rotationPosition) {
-  // char moisture_2 = 0;
-
-  // Wire.beginTransmission(nodeAddress);
-  // Serial.println("go to write W on bus");
-  // Wire.write('W');
-  // Wire.endTransmission(nodeAddress);
-  // return;
-
-  // Request moisture
-  Wire.requestFrom(nodeAddress, 1);
-  Serial.print("Request sent to ");
-  Serial.println(nodeAddress);
-  if (Wire.available()){
-    byte moisture_1 = Wire.read();
-    byte moisture_2 = Wire.read();
-    Serial.print("readed data 1 : ");
-    Serial.println(moisture_1);
-    Serial.print("readed data 2 : ");
-    Serial.println(moisture_2);
-  }
-  else{
-    Serial.println("Wire is not available");
-
-  }
-
-  // if(Wire.available()){
-  //   byte moisture_1 = Wire.read();
-  //   Serial.print("moisture 1 : ");
-  //   Serial.println(moisture_1);
-  // }
-  // else{
-  //   Serial.println("No data received");
-  // }
-  // moisture_2 = Wire.read();
-  // Serial.print("Pot ");
-  // Serial.print(nodeAddress == 0x10 ? 1 : 2);
-  // Serial.print(" - Moisture: ");
-  // Serial.print(moisture_1);
-  // Serial.print((int)moisture_1);
-  // Serial.println(moisture_2);
-  // Serial.print("%, Temperature: ");
-  // Serial.print(temperature);
-  // Serial.println("°C");
-  // char most[3];
-  // most[0] = moisture_1;
-  // most[1] = moisture_2;
-  // most[2] = '\0';
-
-  // int moisture = atoi(most);
-  
-  // int irrigationRate = calculateIrrigationRate(moisture, temperature);
-  
-  // Send irrigation command
-  // Wire.beginTransmission(nodeAddress);
-  // Wire.write('W');
-  // Wire.write(sprintf("%d", irrigationRate));
-  // Wire.endTransmission();
-  
-  // Send rotation command
-  // Wire.beginTransmission(nodeAddress);
-  // Wire.write('R');
-  // Wire.write(rotationPosition);
-  // Wire.endTransmission();
-  
-  // Serial.print("Sent command to Pot ");
-  // Serial.print(nodeAddress == 0x10 ? 1 : 2);
-  // Serial.print(": Irrigation ");
-  // Serial.print(irrigationRate);
-  // Serial.print(" drops/minute, Rotation to ");
-  // Serial.print(rotationPosition);
-  // Serial.println(" degrees");
-}
-
-// int calculateIrrigationRate(int moisture, float temperature) {
-//   if(moisture > 80) {
-//     return 0;
-//   } else if(moisture < 50) {
-//     return 15;
-//   } else {
-//     if(temperature > 25) {
-//       return 10;
-//     } else {
-//       return 5;
-//     }
-//   }
-// }
