d4193cf9b8344711e9cc57c3388e3b8eb9f152f5
SeyyedAhmad <seyyedahmadrokni@ut.ac.ir>
2025-05-02 19:50:28 +0330

    feat: server works but client doesn't work

--- Codes/edge_1/edge_1.ino
+++ Codes/edge_1/edge_1.ino
@@ -1,20 +1,45 @@
-#include <Wire.h>
-#define SLAVE_ADDR 0x08
+#include <EtherCard.h>
 
-byte data_to_send = 150;
+#define REQUEST_INTERVAL 5000
+
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x32 };
+static byte myip[] = { 192, 168, 2, 3 };
+static byte gwip[] = { 192, 168, 2, 1 };
+static byte hisip[] = { 192, 168, 2, 2 }; // IP گره مرکزی
+const char website[] PROGMEM = "192.168.2.2";
+
+byte Ethernet::buffer[500];
+static unsigned long timer = 0;
+
+static void responseCallback (byte status, word off, word len) {
+  Serial.print(F("Reply from master: "));
+  Serial.println((const char*) Ethernet::buffer + off);
+}
 
 void setup() {
-  Wire.begin(SLAVE_ADDR);
-  Serial.begin(9600);
-  Wire.onRequest(sendData);
-  pinMode(A4, INPUT_PULLUP);
-  pinMode(A5, INPUT_PULLUP);
+  Serial.begin(57600);
+  if (!ether.begin(sizeof Ethernet::buffer, mymac, 10)) {
+    Serial.println("Ethernet init failed");
+  }
+
+  ether.staticSetup(myip, gwip);
+  ether.copyIp(ether.hisip, hisip); // مقصد را مشخص کن
+  Serial.println("Edge client ready");
 }
 
 void loop() {
-  delay(1000);
-}
+  ether.packetLoop(ether.packetReceive());
 
-void sendData(){
-  Wire.write(data_to_send);
+  if (millis() - timer > REQUEST_INTERVAL) {
+    timer = millis();
+
+    int moisture = random(30, 90);
+    Serial.print("Sending moisture: ");
+    Serial.println(moisture);
+
+    char url[50];
+    sprintf(url, "/hello/?moisture=%d", moisture);
+
+    ether.browseUrl(PSTR("/hello/"), url + 7, website, responseCallback);
+  }
 }
--- Codes/master_node/master_node.ino
+++ Codes/master_node/master_node.ino
@@ -1,17 +1,56 @@
-#include <Wire.h>
-#define SLAVE_ADDR 0x08
+#include <EtherCard.h>
+
+// MAC و IP این گره
+static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
+static byte myip[] = { 192, 168, 2, 2 };
+static byte gwip[] = { 192, 168, 2, 1 }; // در صورت نیاز، Gateway
+
+byte Ethernet::buffer[700];
+
+// محتوای HTML برای مرورگر
+const char welcomePage[] PROGMEM =
+  "<!DOCTYPE html><html><head><title>Master Node</title></head>"
+  "<body><h1>Welcome to Master Node</h1><p>Status OK</p></body></html>";
 
 void setup() {
-  Wire.begin();
-  Serial.begin(9600);
+  Serial.begin(57600);
+  if (!ether.begin(sizeof Ethernet::buffer, mymac, SS)) {
+    Serial.println("Ethernet init failed");
+  }
+  ether.staticSetup(myip, gwip);
+  Serial.println("Master is ready");
 }
 
 void loop() {
-  Wire.requestFrom(SLAVE_ADDR, 1);
-  if(Wire.available()) {
-    byte data = Wire.read();
-    // Serial.print("Received: ");
+  word len = ether.packetReceive();
+  word pos = ether.packetLoop(len);
+
+  if (pos) {
+    char *data = (char *)Ethernet::buffer + pos;
+    Serial.println("----- FULL REQUEST -----");
     Serial.println(data);
+    Serial.println("------------------------");
+
+    // اگر داده رطوبت داخل URL باشه
+    if (strstr(data, "moisture=")) {
+      char* moistPtr = strstr(data, "moisture=");
+      int moisture = atoi(moistPtr + 9);
+      Serial.print("Moisture received: ");
+      Serial.println(moisture);
+
+      const char* response;
+      if (moisture < 50)
+        response = "START_WATERING";
+      else
+        response = "NO_WATER";
+
+      ether.httpServerReply(strlen(response));
+      memcpy(ether.tcpOffset(), response, strlen(response));
+    }
+    else {
+      // اگر مرورگر درخواست داد → نمایش HTML
+      memcpy_P(ether.tcpOffset(), welcomePage, sizeof welcomePage);
+      ether.httpServerReply(sizeof welcomePage - 1);
+    }
   }
-  delay(1000);
 }
